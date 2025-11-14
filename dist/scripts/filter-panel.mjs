0c1d646671900334d051646fad20e210bea05efe
Tomas Panyokaew <tomaspanyokaew@hotmail.com>
2025-11-14 22:48:39 +0100

    Remove Filter Challenges when panel open

--- dist/scripts/filter-panel.mjs
+++ dist/scripts/filter-panel.mjs
@@ -5,9 +5,11 @@ const filterClose = document.querySelector(".filter-header__close");
 // Öppna panel
 filterToggle.addEventListener("click", () => {
   filterPanel.hidden = false;
+  filterToggle.hidden = true;
 });
 
 // Stänga panel
 filterClose.addEventListener("click", () => {
   filterPanel.hidden = true;
+  filterToggle.hidden = false;
 });
